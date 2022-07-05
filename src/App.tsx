f26a0c303a2edc190a9bfaa36bf4b56acb52f0af
xmNuc <xmnuc@o2.pl>
2022-07-06 00:18:01 +0200

    Add input and btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'primereact/resources/themes/arya-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+
 function App() {
+  const [text, setText] = useState('');
+  const toastRef: any = useRef();
+
+  const onClickBtn = () => {
+    text
+      ? toastRef.current.show({ severity: 'info', summary: 'Success', detail: text })
+      : toastRef.current.show({ severity: 'error', summary: 'Error', detail: 'Value is required' });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Toast ref={toastRef} />
+        <InputText value={text} onChange={(e) => setText(e.target.value)} />
+        {text}
+        <br />
+        <Button type="button" label="Submit" icon="pi pi-check" onClick={onClickBtn} />
       </header>
     </div>
   );
