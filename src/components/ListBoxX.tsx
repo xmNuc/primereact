db7e5ab97ba3f5b497500b7121a5125e5b8197d4
xmNuc <xmnuc@o2.pl>
2022-11-10 00:21:11 +0100

    Add listbox functionality and add array

--- src/components/ListBoxX.tsx
+++ src/components/ListBoxX.tsx
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
-import { ListBox, ListBoxItemTemplateType } from 'primereact/listbox';
-import { data } from './data';
+import { ListBox } from 'primereact/listbox';
+import { data, Idata } from './data';
+import { v4 as uuidv4 } from 'uuid';
 
 export const ListBoxX = () => {
-  const [selectedData, setSelectedData] = useState(null);
+  const [dataArr, setDataArr] = useState<Idata[]>(data);
+  const [selectedData, setSelectedData] = useState<Idata[]>([]);
+  console.log('dataArr', dataArr);
+  console.log('Selected data before', selectedData);
+
+  const handleRemove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, option: Idata) => {
+    console.log('Selected data', selectedData);
+    // e.stopPropagation();
+    const newObj = {
+      id: uuidv4(),
+      name: 'NuC ' + Math.floor(Math.random() * 1000 * 200),
+      price: 65,
+      isins: ['1234', '1234', '1234', '1234'],
+    };
+
+    console.log(
+      'Remove',
+      setDataArr((prev) => prev.filter((obj) => obj.id !== option.id))
+    );
+    setDataArr((prev) => [...prev, newObj]);
+  };
 
   const itemTemplate = (option: any) => {
-    console.log(option.name);
-    return <div>{option.name}</div>;
+    return (
+      <div
+        onClick={(e) => {
+          handleRemove(e, option);
+        }}
+      >
+        {option.name}
+      </div>
+    );
   };
 
   return (
     <div className="card">
       <ListBox
         value={selectedData}
-        options={data}
+        options={dataArr}
         onChange={(e) => setSelectedData(e.value)}
         optionLabel="name"
         filter
