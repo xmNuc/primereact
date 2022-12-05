83d6e89875302577dde5b4293959815a18158958
xmNuc <xmnuc@o2.pl>
2022-12-06 00:20:18 +0100

    Button update

--- src/components/SunEditorComponent.tsx
+++ src/components/SunEditorComponent.tsx
@@ -7,20 +7,36 @@ export const SunEditorComponent = () => {
   return (
     <div>
       <SunEditor
+        min-Height="60vh"
+        width="65vw"
         setOptions={{
           buttonList: [
+            ['font', 'fontSize', 'bold', 'underline', 'italic', 'strike'],
+            ['list', 'align', 'formatBlock', 'table', 'image'],
+            ['undo', 'redo'],
             [
-              'font',
-              'bold',
-              'underline',
-              'italic',
-              'strike',
-              'list',
-              'align',
-              'fontSize',
-              'formatBlock',
-              'table',
-              'image',
+              'paragraphStyle',
+              'blockquote',
+              'subscript',
+              'superscript',
+              'fontColor',
+              'hiliteColor',
+              'textStyle',
+              'removeFormat',
+              'outdent',
+              'indent',
+              'horizontalRule',
+              'lineHeight',
+              'link',
+              'video',
+              'audio',
+              'showBlocks',
+              'codeView',
+              'preview',
+              'print',
+              'save',
+              'template',
+              'fullScreen',
             ],
           ],
         }}
