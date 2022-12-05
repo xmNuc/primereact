e30782d544237ad66405afbe2b372ea86b5d0726
xmNuc <xmnuc@o2.pl>
2022-12-05 23:57:38 +0100

    Add button list

--- src/components/SunEditorComponent.tsx
+++ src/components/SunEditorComponent.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import SunEditor from 'suneditor-react';
 import 'suneditor/dist/css/suneditor.min.css';
+import plugins from 'suneditor/src/plugins';
 
 export const SunEditorComponent = () => {
   return (
     <div>
-      <SunEditor />
+      <SunEditor
+        setOptions={{
+          buttonList: [
+            [
+              'font',
+              'bold',
+              'underline',
+              'italic',
+              'strike',
+              'list',
+              'align',
+              'fontSize',
+              'formatBlock',
+              'table',
+              'image',
+            ],
+          ],
+        }}
+      />
     </div>
   );
 };
