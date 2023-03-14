8e349c6245cd3cf92fe61d29e6436c7ebe49f591
xmNuc <xmnuc@o2.pl>
2023-03-14 21:35:28 +0100

    Update checkbox

--- src/components/InputSw.tsx
+++ src/components/InputSw.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { InputSwitch, InputSwitchChangeEvent } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
 
 export const InputSw = () => {
+  const [checked, setChecked] = useState<boolean>(false);
   const [checked1, setChecked1] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -15,8 +17,9 @@ export const InputSw = () => {
         <h5>Input Switch</h5>
         <InputSwitch
           checked={checked1}
-          onChange={(e: InputSwitchChangeEvent) => setChecked1(e.value)}
+          onChange={(e: InputSwitchChangeEvent) => setChecked1(e.value ?? false)}
         />
+        <Checkbox onChange={(e) => setChecked(e.checked ?? false)} checked={checked}></Checkbox>
       </div>
       <Button label="ClickMe" onClick={handleClick} />
     </div>
