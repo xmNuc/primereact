def0e6dba0e2eabf07552a0f16b6927875ec8915
xmNuc <xmnuc@o2.pl>
2023-03-16 20:13:14 +0100

    Add chips

--- src/components/InputSw.tsx
+++ src/components/InputSw.tsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { InputSwitch, InputSwitchChangeEvent } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
+import { Chips } from 'primereact/chips';
 
 export const InputSw = () => {
   const [checked, setChecked] = useState<boolean>(false);
   const [checked1, setChecked1] = useState<boolean>(false);
+  const [value, setValue] = useState([]);
 
   const handleClick = () => {
     console.log('test');
@@ -20,6 +22,7 @@ export const InputSw = () => {
           onChange={(e: InputSwitchChangeEvent) => setChecked1(e.value ?? false)}
         />
         <Checkbox onChange={(e) => setChecked(e.checked ?? false)} checked={checked}></Checkbox>
+        <Chips value={value} onChange={(e) => setValue(e.value)} separator="," />
       </div>
       <Button label="ClickMe" onClick={handleClick} />
     </div>
