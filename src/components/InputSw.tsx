8628ed1b8f45555fae9ab75c4448fac4e469493d
xmNuc <xmnuc@o2.pl>
2023-03-18 20:20:16 +0100

    add knob

--- src/components/InputSw.tsx
+++ src/components/InputSw.tsx
@@ -3,11 +3,13 @@ import { InputSwitch, InputSwitchChangeEvent } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
 import { Chips } from 'primereact/chips';
+import { Knob } from 'primereact/knob';
 
 export const InputSw = () => {
   const [checked, setChecked] = useState<boolean>(false);
   const [checked1, setChecked1] = useState<boolean>(false);
   const [value, setValue] = useState([]);
+  const [value1, setValue1] = useState([]);
 
   const handleClick = () => {
     console.log('test');
@@ -23,6 +25,7 @@ export const InputSw = () => {
         />
         <Checkbox onChange={(e) => setChecked(e.checked ?? false)} checked={checked}></Checkbox>
         <Chips value={value} onChange={(e) => setValue(e.value)} separator="," />
+        <Knob value={value1} onChange={(e) => setValue1(e.value)} />
       </div>
       <Button label="ClickMe" onClick={handleClick} />
     </div>
