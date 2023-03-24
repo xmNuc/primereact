b2150cad218cb52e4d1c43d792eb7e1a2b360514
xmNuc <xmnuc@o2.pl>
2023-03-24 23:11:40 +0100

    add localeCalendarDemo

---
+++ src/components/LoclaeDemo.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Calendar } from 'primereact/calendar';
+import { addLocale } from 'primereact/api';
+
+export default function LocaleDemo() {
+  const [date, setDate] = useState(null);
+
+  addLocale('es', {
+    firstDayOfWeek: 1,
+    dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+    dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+    dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+    monthNames: [
+      'enero',
+      'febrero',
+      'marzo',
+      'abril',
+      'mayo',
+      'junio',
+      'julio',
+      'agosto',
+      'septiembre',
+      'octubre',
+      'noviembre',
+      'diciembre',
+    ],
+    monthNamesShort: [
+      'ene',
+      'feb',
+      'mar',
+      'abr',
+      'may',
+      'jun',
+      'jul',
+      'ago',
+      'sep',
+      'oct',
+      'nov',
+      'dic',
+    ],
+    today: 'Hoy',
+    clear: 'Limpiar',
+  });
+
+  return (
+    <div className="card flex justify-content-center">
+      <Calendar value={date} onChange={(e) => setDate(e.value)} locale="es" />
+    </div>
+  );
+}
