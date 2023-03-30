d3c852b225e4ae43fa9b08713bafd9485ed95a90
xmNuc <xmnuc@o2.pl>
2023-03-31 00:57:07 +0200

    add ilnine calendar multiple date pick and show week

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Calendar } from 'primereact/calendar';
+import { Calendar, CalendarChangeEvent } from 'primereact/calendar';
 
 export default function IconDoc() {
   const [date, setDate] = useState<string | Date | Date[] | undefined | null>();
@@ -7,6 +7,14 @@ export default function IconDoc() {
   return (
     <div className="card flex justify-content-center">
       <Calendar id="icon" value={date} onChange={(e) => setDate(e.value)} showIcon />
+      <Calendar
+        value={date}
+        onChange={(e: CalendarChangeEvent) => setDate(e.value)}
+        inline
+        showWeek
+        selectionMode="multiple"
+        readOnlyInput
+      />
     </div>
   );
 }
