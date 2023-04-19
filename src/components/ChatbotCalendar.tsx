95b4f1544ff544071b2fb962b2b9703075b206f9
xmNuc <xmnuc@o2.pl>
2023-04-20 00:09:48 +0200

    Add chatbot nav modification

--- src/components/ChatbotCalendar.tsx
+++ src/components/ChatbotCalendar.tsx
@@ -2,6 +2,26 @@ import { addLocale } from 'primereact/api';
 import { Calendar, CalendarDateTemplateEvent } from 'primereact/calendar';
 import './css/calendarChatbot.css';
 
+const arr = [
+  {
+    id: '123',
+    n_date: '2023-04-01',
+    p_date: '2023-04-02',
+  },
+  {
+    id: '1234',
+    n_date: '2023-04-05',
+    p_date: '2023-04-06',
+  },
+];
+
+const newDatesArray = arr.map(({ n_date }) => {
+  const [year, month, day] = n_date.split('-');
+  return [parseInt(day), parseInt(month), parseInt(year)];
+});
+
+console.log(newDatesArray); // [[1, 4, 2023], [5, 4, 2023]]
+
 function dateTemplate(date: CalendarDateTemplateEvent): React.ReactNode {
   const weekend = date.day === 1 || date.day === 6;
   const className = weekend ? 'weekend' : '';
