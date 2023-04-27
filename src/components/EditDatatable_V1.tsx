92968f31272bc38c87cf4d4459921482bcacec7d
xmNuc <xmnuc@o2.pl>
2023-04-27 14:00:30 +0200

    v1 i v2

---
+++ src/components/EditDatatable_V1.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+interface IProduct {
+  id: string | null;
+  product_name: string | null;
+  product_fa_code: string | null;
+  product_alt_fa_code: string | null;
+  other: string | null;
+}
+
+const products: IProduct[] = [
+  {
+    id: '123',
+    product_name: 'name',
+    product_fa_code: 'fa-1',
+    product_alt_fa_code: 'fa-2',
+    other: 'asd',
+  },
+  {
+    id: '456',
+    product_name: 'name2',
+    product_fa_code: 'fa-1_1',
+    product_alt_fa_code: 'fa-2_2',
+    other: 'asd_2',
+  },
+];
+
+const App = () => {
+  const [productData, setProductData] = useState<IProduct[]>(products);
+
+  const onProductFaCodeEditor = (rowData: IProduct) => {
+    return (
+      <InputText
+        type="text"
+        value={rowData.product_fa_code || ''}
+        onChange={(e) => {
+          const newProductData = [...productData];
+          const index = newProductData.findIndex((product) => product.id === rowData.id);
+          newProductData[index].product_fa_code = e.target.value;
+          setProductData(newProductData);
+        }}
+      />
+    );
+  };
+
+  const onProductAltFaCodeEditor = (rowData: IProduct) => {
+    return (
+      <InputText
+        type="text"
+        value={rowData.product_alt_fa_code || ''}
+        onChange={(e) => {
+          const newProductData = [...productData];
+          const index = newProductData.findIndex((product) => product.id === rowData.id);
+          newProductData[index].product_alt_fa_code = e.target.value;
+          setProductData(newProductData);
+        }}
+      />
+    );
+  };
+
+  const onSaveButtonClick = () => {
+    const newProducts: IProduct[] = [];
+    productData.forEach((product) => {
+      const newProduct: IProduct = {
+        ...product,
+        product_fa_code: product.product_fa_code || '',
+        product_alt_fa_code: product.product_alt_fa_code || '',
+      };
+      newProducts.push(newProduct);
+    });
+    console.log(newProducts);
+  };
+
+  return (
+    <>
+      <DataTable value={productData} editMode="row">
+        <Column field="product_name" header="Product Name" />
+        <Column field="product_fa_code" header="Product FA Code" editor={onProductFaCodeEditor} />
+        <Column
+          field="product_alt_fa_code"
+          header="Product Alt FA Code"
+          editor={onProductAltFaCodeEditor}
+        />
+      </DataTable>
+      <Button label="Save" onClick={onSaveButtonClick} />
+    </>
+  );
+};
+
+export default App;
